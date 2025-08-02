b114cb5d95e24d7546a6eb2efa6a6b6260336f06
Andrey Okunev <88597889+33012287a@users.noreply.github.com>
2025-08-02 21:35:07 +0000

    add CRUD

--- user-service/src/main/java/com/astondev/app/App.java
+++ user-service/src/main/java/com/astondev/app/App.java
@@ -1,12 +1,102 @@
 package com.astondev.app;
 
+import java.util.List;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 public class App {
+    private final SessionFactory sessionFactory;
+
+    public App() {
+        this.sessionFactory = new Configuration()
+            .configure()
+            .buildSessionFactory();
+    }
+
+    public List<Users> getAllUsers() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("from Users", Users.class).list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void addUser(Users user) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(user);
+            session.flush();
+            transaction.commit();
+            System.out.println("Добавлен новый юзер: " + user.getName());
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public Users getUserById(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            Users user = session.find(Users.class, id);
+            return user;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean saveUser(Users user) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean removeUser(Users user) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.remove(user);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return false;
+    }
+    
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-        System.out.println("SessionFactory created: " + sessionFactory);
+        App app = new App();
+
+        Users newUser = new Users("Petr", "asqq@ss.com");
+        app.addUser(newUser);
+        System.out.println(newUser.toString());
+
+        List<Users> users = app.getAllUsers();
+        if (users != null) {
+            for (Users user : users) {
+                System.out.println("User ID: " + user.getId() + ", Name: " + user.getName() + ", Email: " + user.getEmail());
+            }
+        }
     }
 }
--- user-service/src/main/java/com/astondev/app/Users.java
+++ user-service/src/main/java/com/astondev/app/Users.java
@@ -2,25 +2,78 @@ package com.astondev.app;
 
 import java.util.Date;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="users")
 public class Users {
     @Id
-    @Column(name="id")
-    public Integer id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_seq")
+    @SequenceGenerator(name = "users_id_seq", sequenceName = "users_id_seq", allocationSize = 1)
+    @Column(name = "id")
+    private Integer id;
 
     @Column(name="name")
-    public String name;
+    private String name;
 
     @Column(name = "email")
-    public String email;
+    private String email;
 
     @Column(name="create_at", nullable = false)
-    public Date create_at;
- 
+    @CreationTimestamp
+    private Date create_at;
+    
+
+    public Users() {}
+
+    public Users(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    public Date getCreateAt() {
+        return create_at;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", email='" + email + '\'' +
+            ", create_at=" + create_at +
+            '}';
+    }
 }
