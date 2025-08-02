967b1ff0119c11920038a68af08edbcef3646cf1
Andrey Okunev <88597889+33012287a@users.noreply.github.com>
2025-08-02 16:42:20 +0000

    create sesion

--- user-service/src/main/java/com/astondev/app/App.java
+++ user-service/src/main/java/com/astondev/app/App.java
@@ -1,10 +1,12 @@
 package com.astondev.app;
 
-/**
- * Hello world!
- */
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        System.out.println("SessionFactory created: " + sessionFactory);
     }
 }
--- user-service/src/main/java/com/astondev/app/Users.java
+++ user-service/src/main/java/com/astondev/app/Users.java
@@ -1,11 +1,26 @@
 package com.astondev.app;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
 @Table(name="users")
 public class Users {
-    public int id;
+    @Id
+    @Column(name="id")
+    public Integer id;
+
+    @Column(name="name")
     public String name;
+
+    @Column(name = "email")
     public String email;
+
+    @Column(name="create_at", nullable = false)
     public Date create_at;
  
 }
